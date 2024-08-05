db13b7058b84fec6db53f8ac0dcc8f182c1da862
emShalaby <medo.dice@gmail.com>
2024-08-05 20:31:20 +0300

    add sticky header and make the layout specific to medium device screens

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,4 +1,5 @@
 export  function loadHeader(active){
+  const $header=$('<div>');
   const $container=$('<div>');
   const $icon=$('<img>');
   const $pageTitles=$('<ul>');
@@ -17,22 +18,29 @@ export  function loadHeader(active){
   const $containerLeft=$('<div>');
   const $containerRight=$('<div>');
   const $activeList=$('<span>');
+  const $stickyHeader=$('<div>');
+  const $smJoin=$('<button>');
+  $header.attr('class','flex flex-col');
+  $smJoin.attr('class','px-5 py-1 rounded-full border border-white text-white text-sm font-bold hover:bg-black transition duration-300 md:hidden');
+  $stickyHeader.attr('class','sticky-header px-8 py-4 font-bold sticky flex justify-between');
+  $stickyHeader.text('STARBUCKS® REWARDS');
+  $smJoin.text('Join in the app');
   $container.attr('class','container px-10 flex items-center md:flex-row  md:justify-between ');
   $containerLeft.attr('class','flex gap-4 ');
   $containerRight.attr('class','flex gap-4');
   $icon.attr('src','src/assets/svgs/starbucks_icon.svg');
   $icon.attr('class','flex items-center m-2 my-6');
-  $pageTitles.attr('class','flex gap-6 ml-3 font-bold text-sm');
-  $rewards.attr('class','flex items-center relative');
-  $giftCards.attr('class','flex items-center relative');
-  $menu.attr('class','flex items-center relative');
-  $location.attr('class','flex items-center gap-2 mr-10 ');
+  $pageTitles.attr('class','hidden md:flex  ml-3 font-bold text-sm gap-3');
+  $rewards.attr('class','flex items-center relative px-1');
+  $giftCards.attr('class','flex items-center relative px-1');
+  $menu.attr('class','flex items-center relative px-1');
+  $location.attr('class',' hidden md:flex items-center gap-2 mr-10 ');
   $locationImg.attr('src','src/assets/svgs/location_icon.svg');
   $signing.attr('class','gap-4 flex items-center');
-  $signIn.attr('class','px-5 py-1 rounded-full border border-black text-black text-xs font-bold hover:bg-gray-100 transition duration-300');
-  $signUp.attr('class','px-5 py-1 rounded-full bg-black text-white rounded-full text-xs transition duration font-bold ');
-  $activeList.attr('class','absolute bottom-0 left-0 w-full h-2  block active');
-  $locationText.attr('class','font-bold text-xs');
+  $signIn.attr('class','hidden px-5 py-1 rounded-full border border-black text-black text-sm font-bold hover:bg-gray-100 transition duration-300 md:flex');
+  $signUp.attr('class','hidden px-5 py-1 rounded-full bg-black text-white rounded-full text-sm transition duration font-bold md:flex');
+  $activeList.attr('class','absolute bottom-0 left-0 w-full  block active');
+  $locationText.attr('class','font-bold text-sm');
   
   $locationImg.height('30');
   $locationImg.width('25');
@@ -60,7 +68,10 @@ export  function loadHeader(active){
   $containerLeft.append($pageTitles);
   $container.append($containerLeft);
   $container.append($containerRight);
-  $('#app').append($container);
+  $stickyHeader.append($smJoin);
+  $header.append($container);
+  $header.append($stickyHeader);
+  $('#app').append($header);
   if(active=='rewards'){
     
     if($menu.children('.active'))$menu.children('.active').remove;
