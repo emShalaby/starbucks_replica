c581a04d10eb09a8bc7a1ec1b946762b044a258a
emShalaby <medo.dice@gmail.com>
2024-08-06 18:32:44 +0300

    improve footer layout for both desktop and mobile

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,88 +1,107 @@
-export function loadFooter(){
-  const $footer=$('<div>');
-  const $topContainer=$('<div>');
-  const $bottomContainer=$('<div>');
-  const $aboutUs=$('<div>');
-  const $careers=$('<div>');
-  const $socialImpact=$('<div>');
-  const $businessPartners=$('<div>');
-  const $order=$('<div>');
-  const $aboutUsH2=$('<h2>');
-  const $careersH2=$('<h2>');
-  const $socialImpactH2=$('<h2>');
-  const $businessPartnersH2=$('<h2>');
-  const $orderH2=$('<h2>');
-  const $ourCompany=$('<a>');
-  const $ourCoffe=$('<a>');
-  const $stories =$('<a>');
-  const $archive =$('<a>');
-  const $investors =$('<a>');
-  const $customerService =$('<a>');
-  const $contacts =$('<a>');
-  const $aboutUsUl=$('<ul>');
-  const $careersUl=$('<ul>');
-  const $culture=$('<a>');
-  const $inclusion=$('<a>');
-  const $college=$('<a>');
-  const $alumni=$('<a>');
-  const $usCareers=$('<a>');
-  const $internationalCareers=$('<a>');
-  const $socialImpactUl=$('<ul>');
-  const $people=$('<a>');
-  const $planet=$('<a>');
-  const $environment=$('<a>');
-  const $businessPartnersUl=$('<ul>');
-  const $landlord=$('<a>');
-  const $suppliers=$('<a>');
-  const $giftSales=$('<a>');
-  const $office=$('<a>');
-  const $orderUl=$('<ul>');
-  const $appOrder=$('<a>');
-  const $webOrder=$('<a>');
-  const $delivery=$('<a>');
-  const $orderOptions=$('<a>');
-  const $orderExploring=$('<a>');
-  const $arrowDown=$('<img>');
-  const $socials=$('<div>');
-  const $spotify=$('<a>');
-  const $facebook=$('<a>');
-  const $pintrest=$('<a>');
-  const $instagram=$('<a>');
-  const $youtube=$('<a>');
-  const $twitter=$('<a>');
-  const $pintrestIcon=$('<img>');
-  const $spotifyIcon=$('<img>');
-  const $facebookIcon=$('<img>');
-  const $instagramIcon  =$('<img>');
-  const $youtubeIcon=$('<img>');
-  const $twitterIcon=$('<img>');
-
-  $socials.attr('class','flex p-5 gap-4 socials');
-  $twitterIcon.attr('src','src/assets/svgs/twitter-icon.svg');
-  $youtubeIcon.attr('src','src/assets/svgs/youtube-icon.svg');
-  $instagramIcon.attr('src','src/assets/svgs/instagram-icon.svg');
-  $pintrestIcon.attr('src','src/assets/svgs/pintrest-icon.svg');
-  $facebookIcon.attr('src','src/assets/svgs/facebook-icon.svg');
-  $spotifyIcon.attr('src','src/assets/svgs/spotify-icon.svg');
-  $arrowDown.attr({'src':'src/assets/svgs/arrow-down.svg','class':'md:hidden'});
-  $footer.attr('class','flex flex-col');
-  $topContainer.attr('class','flex flex-col p-10 gap-16 md:grid md:grid-cols-5 md:py-10 md:pl-8 lg:pr-96 md:gap-6 footer-top');
-  $bottomContainer.attr('class','flex flex-col footer-bottom');
-  $aboutUs.attr('class','flex flex-col md:gap-10 gap-8 hover:cursor-pointer' );
-  $careers.attr('class','flex flex-col md:gap-10 gap-8 hover:cursor-pointer');
-  $socialImpact.attr('class','flex flex-col md:gap-10 gap-8 hover:cursor-pointer');
-  $businessPartners.attr('class','flex flex-col md:gap-10 gap-8 hover:cursor-pointer');
-  $order.attr('class','flex flex-col md:gap-10 gap-8 hover:cursor-pointer');
-  $aboutUsH2.attr('class','text-lg font-semibold flex justify-between');
-  $careersH2.attr('class','text-lg font-semibold flex justify-between');
-  $socialImpactH2.attr('class','text-lg font-semibold flex justify-between');
-  $businessPartnersH2.attr('class','text-lg font-semibold flex justify-between');
-  $orderH2.attr('class','text-lg font-semibold flex justify-between');
-  $aboutUsUl.attr('class',' hidden flex  md:flex flex-col gap-6');
-  $careersUl.attr('class','hidden flex  md:flex flex-col gap-6');
-  $socialImpactUl.attr('class',' hidden flex md:flex flex-col gap-6');
-  $businessPartnersUl.attr('class',' hidden flex md:flex flex-col gap-6');
-  $orderUl.attr('class','hidden flex md:flex flex-col gap-6');
+export function loadFooter() {
+  const $footer = $('<div>');
+  const $topContainer = $('<div>');
+  const $bottomContainer = $('<div>');
+  const $aboutUs = $('<div>');
+  const $careers = $('<div>');
+  const $socialImpact = $('<div>');
+  const $businessPartners = $('<div>');
+  const $order = $('<div>');
+  const $aboutUsH2 = $('<h2>');
+  const $careersH2 = $('<h2>');
+  const $socialImpactH2 = $('<h2>');
+  const $businessPartnersH2 = $('<h2>');
+  const $orderH2 = $('<h2>');
+  const $ourCompany = $('<a>');
+  const $ourCoffe = $('<a>');
+  const $stories = $('<a>');
+  const $archive = $('<a>');
+  const $investors = $('<a>');
+  const $customerService = $('<a>');
+  const $contacts = $('<a>');
+  const $aboutUsUl = $('<ul>');
+  const $careersUl = $('<ul>');
+  const $culture = $('<a>');
+  const $inclusion = $('<a>');
+  const $college = $('<a>');
+  const $alumni = $('<a>');
+  const $usCareers = $('<a>');
+  const $internationalCareers = $('<a>');
+  const $socialImpactUl = $('<ul>');
+  const $people = $('<a>');
+  const $planet = $('<a>');
+  const $environment = $('<a>');
+  const $businessPartnersUl = $('<ul>');
+  const $landlord = $('<a>');
+  const $suppliers = $('<a>');
+  const $giftSales = $('<a>');
+  const $office = $('<a>');
+  const $orderUl = $('<ul>');
+  const $appOrder = $('<a>');
+  const $webOrder = $('<a>');
+  const $delivery = $('<a>');
+  const $orderOptions = $('<a>');
+  const $orderExploring = $('<a>');
+  const $arrowDown = $('<img>');
+  const $socials = $('<div>');
+  const $spotify = $('<a>');
+  const $facebook = $('<a>');
+  const $pintrest = $('<a>');
+  const $instagram = $('<a>');
+  const $youtube = $('<a>');
+  const $twitter = $('<a>');
+  const $pintrestIcon = $('<img>');
+  const $spotifyIcon = $('<img>');
+  const $facebookIcon = $('<img>');
+  const $instagramIcon = $('<img>');
+  const $youtubeIcon = $('<img>');
+  const $twitterIcon = $('<img>');
+  const $notices = $('<div>');
+  const $notice = $('<a>');
+  const $footNote = $('<p>');
+  $footNote.attr('class','p-7 text-xs')
+  $socials.attr('class', 'flex p-5 gap-2 socials');
+  $twitterIcon.attr('src', 'src/assets/svgs/twitter-icon.svg');
+  $youtubeIcon.attr('src', 'src/assets/svgs/youtube-icon.svg');
+  $instagramIcon.attr('src', 'src/assets/svgs/instagram-icon.svg');
+  $pintrestIcon.attr('src', 'src/assets/svgs/pintrest-icon.svg');
+  $facebookIcon.attr('src', 'src/assets/svgs/facebook-icon.svg');
+  $spotifyIcon.attr('src', 'src/assets/svgs/spotify-icon.svg');
+  $notices.attr('class', 'flex flex-col px-7 gap-5');
+  $arrowDown.attr({
+    src: 'src/assets/svgs/arrow-down.svg',
+    class: 'md:hidden',
+  });
+  $footer.attr('class', 'flex flex-col');
+  $topContainer.attr(
+    'class',
+    'flex flex-col p-10 gap-16 md:grid md:grid-cols-5 md:py-10 md:pl-8 lg:pr-96 md:gap-6 footer-top '
+  );
+  $bottomContainer.attr('class', 'flex flex-col footer-bottom gap-3  pb-40');
+  $aboutUs.attr('class', 'flex flex-col md:gap-10 gap-8 hover:cursor-pointer');
+  $careers.attr('class', 'flex flex-col md:gap-10 gap-8 hover:cursor-pointer');
+  $socialImpact.attr(
+    'class',
+    'flex flex-col md:gap-10 gap-8 hover:cursor-pointer'
+  );
+  $businessPartners.attr(
+    'class',
+    'flex flex-col md:gap-10 gap-8 hover:cursor-pointer'
+  );
+  $order.attr('class', 'flex flex-col md:gap-10 gap-8 hover:cursor-pointer');
+  $aboutUsH2.attr('class', 'text-lg font-semibold flex justify-between');
+  $careersH2.attr('class', 'text-lg font-semibold flex justify-between');
+  $socialImpactH2.attr('class', 'text-lg font-semibold flex justify-between');
+  $businessPartnersH2.attr(
+    'class',
+    'text-lg font-semibold flex justify-between'
+  );
+  $orderH2.attr('class', 'text-lg font-semibold flex justify-between');
+  $aboutUsUl.attr('class', ' hidden flex  md:flex flex-col gap-6');
+  $careersUl.attr('class', 'hidden flex  md:flex flex-col gap-6');
+  $socialImpactUl.attr('class', ' hidden flex md:flex flex-col gap-6');
+  $businessPartnersUl.attr('class', ' hidden flex md:flex flex-col gap-6');
+  $orderUl.attr('class', 'hidden flex md:flex flex-col gap-6');
 
   $aboutUsH2.text('About Us');
   $careersH2.text('Careers');
@@ -149,7 +168,7 @@ export function loadFooter(){
   $careersUl.append($usCareers);
   $careersUl.append($internationalCareers);
   $socialImpact.append($socialImpactUl);
-  $socialImpactUl.append($people);  
+  $socialImpactUl.append($people);
   $socialImpactUl.append($planet);
   $socialImpactUl.append($environment);
   $businessPartners.append($businessPartnersUl);
@@ -163,31 +182,48 @@ export function loadFooter(){
   $orderUl.append($delivery);
   $orderUl.append($orderOptions);
   $orderUl.append($orderExploring);
-  $bottomContainer.append($socials);
-
-  $socials.append($spotify,$facebook,$pintrest,$instagram,$youtube,$twitter);
+  $bottomContainer.append(
+    $socials,
+    $notices,
+    $footNote.text('© 2024 Starbucks Coffee Company. All rights reserved.')
+  );
+  $socials.append(
+    $spotify,
+    $facebook,
+    $pintrest,
+    $instagram,
+    $youtube,
+    $twitter
+  );
   $spotify.append($spotifyIcon);
   $facebook.append($facebookIcon);
   $pintrest.append($pintrestIcon);
   $instagram.append($instagramIcon);
   $youtube.append($youtubeIcon);
   $twitter.append($twitterIcon);
-  $aboutUsH2.on('click',()=>{
-  
+  $notices.append(
+    $notice.text('Privacy Notice'),
+    $notice.clone().text('Consumer Health Privacy Notice'),
+    $notice.clone().text('Terms of Use'),
+    $notice.clone().text('Do Not Share My Personal Information'),
+    $notice.clone().text('CA supply Chain Act'),
+    $notice.clone().text('Accessibility'),
+    $notice.clone().text('Cookie Preferences')
+  );
+
+  $aboutUsH2.on('click', () => {
     $aboutUsUl.toggleClass('hidden');
-    
   });
-  $careersH2.on('click',()=>{
+  $careersH2.on('click', () => {
     $careersUl.toggleClass('hidden');
   });
-  $socialImpactH2.on('click',()=>{
+  $socialImpactH2.on('click', () => {
     $socialImpactUl.toggleClass('hidden');
   });
-  $businessPartnersH2.on('click',()=>{
+  $businessPartnersH2.on('click', () => {
     $businessPartnersUl.toggleClass('hidden');
   });
-  $orderH2.on('click',()=>{
+  $orderH2.on('click', () => {
     $orderUl.toggleClass('hidden');
   });
-  
 }
--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,75 +1,102 @@
-export  function loadHeader(active){
-  const $header=$('<div>');
-  const $container=$('<div>');
-  const $icon=$('<img>');
-  const $pageTitles=$('<ul>');
-  const $menu=$('<li>');
-  const $rewards=$('<li>');
-  const $giftCards=$('<li>');
-  const $menuLink=$('<a>');
-  const $rewardsLink=$('<a>');
-  const $giftCardsLink=$('<a>');
-  const $signing=$('<div>');
-  const $signIn=$('<button>');
-  const $signUp=$('<button>');
-  const $location=$('<div>');
-  const $locationImg=$('<img>');
-  const $locationText=$('<h5>');
-  const $containerLeft=$('<div>');
-  const $containerRight=$('<div>');
-  const $activeList=$('<span>');
-  const $stickyHeader=$('<div>');
-  const $smJoin=$('<button>');
-  const $moreIcon=$('<img>');
-  const $smNav=$('<div>');
-  const $smPagetitles=$('<ul>');
-  const $smMenu=$('<li>');
-  const $smRewards=$('<li>');
-  const $smGiftCards=$('<li>');
-  const $smBtngroup=$('<div>');
-  const $smSignInBtn=$('<button>');
-  const $smSignUpBtn=$('<button>');
-  const $smLocation=$('<div>');
-  const $smLocationText=$('<div>');
-  const $smLocationIcon=$('<img>');
-  const $closeIcon=$('<img>');
+export function loadHeader(active) {
+  const $header = $('<div>');
+  const $container = $('<div>');
+  const $icon = $('<img>');
+  const $pageTitles = $('<ul>');
+  const $menu = $('<li>');
+  const $rewards = $('<li>');
+  const $giftCards = $('<li>');
+  const $menuLink = $('<a>');
+  const $rewardsLink = $('<a>');
+  const $giftCardsLink = $('<a>');
+  const $signing = $('<div>');
+  const $signIn = $('<button>');
+  const $signUp = $('<button>');
+  const $location = $('<div>');
+  const $locationImg = $('<img>');
+  const $locationText = $('<h5>');
+  const $containerLeft = $('<div>');
+  const $containerRight = $('<div>');
+  const $activeList = $('<span>');
+  const $stickyHeader = $('<div>');
+  const $smJoin = $('<button>');
+  const $moreIcon = $('<img>');
+  const $smNav = $('<div>');
+  const $smPagetitles = $('<ul>');
+  const $smMenu = $('<li>');
+  const $smRewards = $('<li>');
+  const $smGiftCards = $('<li>');
+  const $smBtngroup = $('<div>');
+  const $smSignInBtn = $('<button>');
+  const $smSignUpBtn = $('<button>');
+  const $smLocation = $('<div>');
+  const $smLocationText = $('<div>');
+  const $smLocationIcon = $('<img>');
+  const $closeIcon = $('<img>');
 
-  $smNav.attr('class','translate-x-full transition-transform duration-500 ease-in-out flex flex-col fixed self-end px-3 bg-white h-full md:hidden sm-nav w-4/5 ');
-  $moreIcon.attr('class','more-icon block md:hidden');
-  $header.attr('class','flex flex-col');
-  $smJoin.attr('class','px-5 py-1 rounded-full border border-white text-white text-sm font-bold hover:bg-black transition duration-300 md:hidden');
-  $stickyHeader.attr('class','sticky-header md:px-8 px-5 py-4 font-bold sticky flex justify-between');
+  $smNav.attr(
+    'class',
+    'translate-x-full transition-transform duration-500 ease-in-out flex flex-col fixed self-end px-3 bg-white h-full md:hidden sm-nav w-4/5 '
+  );
+  $moreIcon.attr('class', 'more-icon block md:hidden');
+  $header.attr('class', 'flex flex-col');
+  $smJoin.attr(
+    'class',
+    'px-5 py-1 rounded-full border border-white text-white text-sm font-bold hover:bg-black transition duration-300 md:hidden'
+  );
+  $stickyHeader.attr(
+    'class',
+    'sticky-header md:px-8 px-5 py-4 font-bold sticky flex justify-between'
+  );
   $stickyHeader.text('STARBUCKS® REWARDS');
   $smJoin.text('Join in the app');
-  $container.attr('class','justify-between px-10 flex items-center md:flex-row   ');
-  $containerLeft.attr('class','flex gap-4 ');
-  $containerRight.attr('class','flex gap-4');
-  $icon.attr('class','flex items-center m-2 my-6');
-  $pageTitles.attr('class','hidden md:flex  ml-3 font-bold text-sm gap-3');
-  $rewards.attr('class','flex items-center relative px-1');
-  $giftCards.attr('class','flex items-center relative px-1');
-  $menu.attr('class','flex items-center relative px-1');
-  $location.attr('class',' hidden md:flex items-center gap-2 mr-10 ');
-  $signing.attr('class','gap-4 flex items-center');
-  $signIn.attr('class','hidden px-5 py-1 rounded-full border border-black text-black text-sm font-bold hover:bg-gray-100 transition duration-300 md:flex');
-  $signUp.attr('class','hidden px-5 py-1 rounded-full bg-black text-white rounded-full text-sm font-bold md:flex sign-up-btn');
-  $activeList.attr('class','absolute bottom-0 left-0 w-full  block active');
-  $locationText.attr('class','font-bold text-sm location-text');
-  $smPagetitles.attr('class','flex flex-col  font-bold  pb-8 mt-4 sm-page-titles');
-  $smMenu.attr('class','px-6 py-5');
-  $smRewards.attr('class','px-6 py-5');
-  $smGiftCards.attr('class','px-6 py-5 ');
-  $smSignInBtn.attr('class',' px-5 py-1 rounded-full border border-black text-black text-sm font-bold hover:bg-gray-100 transition duration-300 md:flex');
-  $smBtngroup.attr('class','mt-8 flex gap-5 px-3');
-  $smSignUpBtn.attr('class',' px-5 py-1 rounded-full bg-black text-white rounded-full text-sm font-bold md:flex sign-up-btn');
-  $smLocation.attr('class','flex gap-3 mt-5 px-3 sm-location-text');
-  $closeIcon.attr('class','hidden md:hidden close-icon');
+  $container.attr(
+    'class',
+    'justify-between px-10 flex items-center md:flex-row   '
+  );
+  $containerLeft.attr('class', 'flex gap-4 ');
+  $containerRight.attr('class', 'flex gap-4');
+  $icon.attr('class', 'flex items-center m-2 my-6');
+  $pageTitles.attr('class', 'hidden md:flex  ml-3 font-bold text-sm gap-3');
+  $rewards.attr('class', 'flex items-center relative px-1');
+  $giftCards.attr('class', 'flex items-center relative px-1');
+  $menu.attr('class', 'flex items-center relative px-1');
+  $location.attr('class', ' hidden md:flex items-center gap-2 mr-10 ');
+  $signing.attr('class', 'gap-4 flex items-center');
+  $signIn.attr(
+    'class',
+    'hidden px-5 py-1 rounded-full border border-black text-black text-sm font-bold hover:bg-gray-100 transition duration-300 md:flex'
+  );
+  $signUp.attr(
+    'class',
+    'hidden px-5 py-1 rounded-full bg-black text-white rounded-full text-sm font-bold md:flex sign-up-btn'
+  );
+  $activeList.attr('class', 'absolute bottom-0 left-0 w-full  block active');
+  $locationText.attr('class', 'font-bold text-sm location-text');
+  $smPagetitles.attr(
+    'class',
+    'flex flex-col  font-bold  pb-8 mt-4 sm-page-titles'
+  );
+  $smMenu.attr('class', 'px-6 py-5');
+  $smRewards.attr('class', 'px-6 py-5');
+  $smGiftCards.attr('class', 'px-6 py-5 ');
+  $smSignInBtn.attr(
+    'class',
+    ' px-5 py-1 rounded-full border border-black text-black text-sm font-bold hover:bg-gray-100 transition duration-300 md:flex'
+  );
+  $smBtngroup.attr('class', 'mt-8 flex gap-5 px-3');
+  $smSignUpBtn.attr(
+    'class',
+    ' px-5 py-1 rounded-full bg-black text-white rounded-full text-sm font-bold md:flex sign-up-btn'
+  );
+  $smLocation.attr('class', 'flex gap-3 mt-5 px-3 sm-location-text');
+  $closeIcon.attr('class', 'hidden md:hidden close-icon');
 
-  $moreIcon.attr('src','src/assets/svgs/more.svg');
-  $icon.attr('src','src/assets/svgs/starbucks_icon.svg');
-  $locationImg.attr('src','src/assets/svgs/location_icon.svg');
-  $smLocationIcon.attr('src','src/assets/svgs/location_icon.svg');
-  $closeIcon.attr('src','src/assets/svgs/close-icon.svg');
+  $moreIcon.attr('src', 'src/assets/svgs/more.svg');
+  $icon.attr('src', 'src/assets/svgs/starbucks_icon.svg');
+  $locationImg.attr('src', 'src/assets/svgs/location_icon.svg');
+  $smLocationIcon.attr('src', 'src/assets/svgs/location_icon.svg');
+  $closeIcon.attr('src', 'src/assets/svgs/close-icon.svg');
 
   $header.append($container);
   $header.append($stickyHeader);
@@ -94,7 +121,7 @@ export  function loadHeader(active){
   $smGiftCards.text('GIFT CARDS');
   $smRewards.text('REWARDS');
   $smMenu.text('MENU');
- 
+
   $locationImg.height('30');
   $locationImg.width('25');
   $icon.height('52');
@@ -102,61 +129,52 @@ export  function loadHeader(active){
   $locationText.text('Find a store');
   $signIn.text('Sign In');
   $signUp.text('Join Now');
-  $signing.append($signIn,$signUp);
+  $signing.append($signIn, $signUp);
   $menuLink.text('MENU');
   $giftCardsLink.text('GIFT CARDS');
-  
+
   $rewardsLink.text('REWARDS');
   $rewards.append($rewardsLink);
   $rewards.append($activeList);
   $menu.append($menuLink);
   $giftCards.append($giftCardsLink);
-  $pageTitles.append($menu,$rewards,$giftCards);
+  $pageTitles.append($menu, $rewards, $giftCards);
   $location.append($locationImg);
   $location.append($locationText);
- 
 
   $stickyHeader.append($smJoin);
-  
-
- 
 
   $('#app').append($header);
-  if(active=='rewards'){
-    
-    if($menu.children('.active'))$menu.children('.active').remove;
-    if($menu.children('.active'))$menu.children('.active').remove();
-    if($menu.children('.active'))$menu.children('.active').remove();
+  if (active == 'rewards') {
+    if ($menu.children('.active')) $menu.children('.active').remove;
+    if ($menu.children('.active')) $menu.children('.active').remove();
+    if ($menu.children('.active')) $menu.children('.active').remove();
     $rewards.append($activeList);
-  }
-  else if(active=='menu'){
-    if($menu.children('.active'))$menu.children('.active').remove;
-    if($menu.children('.active'))$menu.children('.active').remove();
-    if($menu.children('.active'))$menu.children('.active').remove();
+  } else if (active == 'menu') {
+    if ($menu.children('.active')) $menu.children('.active').remove;
+    if ($menu.children('.active')) $menu.children('.active').remove();
+    if ($menu.children('.active')) $menu.children('.active').remove();
     $menu.append($activeList);
     $stickyHeader.addClass('hidden');
-
-  }
-  else{
-    if($menu.children('.active'))$menu.children('.active').remove;
-    if($menu.children('.active'))$menu.children('.active').remove();
-    if($menu.children('.active'))$menu.children('.active').remove();
+  } else {
+    if ($menu.children('.active')) $menu.children('.active').remove;
+    if ($menu.children('.active')) $menu.children('.active').remove();
+    if ($menu.children('.active')) $menu.children('.active').remove();
     $giftCards.append($activeList);
     $stickyHeader.addClass('hidden');
-
   }
-  $('html').on('click',()=> {
+  $('html').on('click', () => {
     if (!$smNav.hasClass('hidden')) $smNav.addClass('translate-x-full');
     if (!$closeIcon.hasClass('hidden')) $closeIcon.toggleClass('hidden');
     if ($moreIcon.hasClass('hidden')) $moreIcon.toggleClass('hidden');
   });
-  $moreIcon.parent().on('click',(e)=>e.stopPropagation());
-  $moreIcon.on('click',()=>{
+  $moreIcon.parent().on('click', (e) => e.stopPropagation());
+  $moreIcon.on('click', () => {
     $smNav.toggleClass('translate-x-full');
     $closeIcon.toggleClass('hidden');
-    $moreIcon.toggleClass('hidden')
-    ;});
-  $closeIcon.on('click',()=>{
+    $moreIcon.toggleClass('hidden');
+  });
+  $closeIcon.on('click', () => {
     $smNav.toggleClass('translate-x-full');
     $closeIcon.toggleClass('hidden');
     $moreIcon.toggleClass('hidden');
@@ -166,11 +184,10 @@ export  function loadHeader(active){
   $smSignInBtn.text('Sign in');
 
   $smSignUpBtn.text('Join Now');
-  
+
   $smLocationIcon.width('20');
   $smNav.append($smLocation);
   $smLocationText.text('Find a store');
   $closeIcon.height('40');
   $closeIcon.width(20);
-  
 }
