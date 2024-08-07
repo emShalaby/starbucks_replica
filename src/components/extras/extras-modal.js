a9511abeb94328d1be9d0ced6dfce0268ccff039
emShalaby <medo.dice@gmail.com>
2024-08-07 23:06:10 +0300

    basic extras layout

---
+++ src/components/extras/extras-modal.js
@@ -0,0 +1,59 @@
+export function loadExtrasModal(imgs, headers, text) {
+  const $layout = $('<div>');
+  const $modal = $('<div>');
+  const $img = $('<img>');
+  const $header = $('<h2>');
+  const $p = $('<p>');
+  const $close = $('<img>');
+  const $content = $('<div>');
+  const $control = $('<div>');
+  const $circleButton=$('<button>');
+  $circleButton.width('12');
+  $circleButton.height('12');
+  $content.addClass('content');
+  $img.height('200');
+  $img.width('200');
+  $close.width('24');
+  $close.height('24');
+  $circleButton.addClass('rounded-full bg-black');
+  $close.attr('src', 'src/assets/svgs/close-icon.svg');
+  $close.addClass('hover:cursor-pointer');
+  $layout.addClass(
+    'top-0 left-0 w-[100vw] h-[100vh] position fixed bg-[#00000080] flex justify-center place-items-center'
+  );
+  $modal.addClass('bg-white flex flex-col  p-3 pb-8 text-center w-[26.5rem] h-[26.5rem]  place-items-center rounded-lg');
+  $content.addClass('flex flex-col gap-5 place-items-center');
+  $close.addClass('self-end');
+  $header.addClass('font-semibold text-xl');
+  $p.addClass('text-sm');
+  $control.addClass('mt-auto flex gap-3');
+  $modal.append($close);
+  for (let i = 0; i < imgs.length; i++) {
+    $modal.append(
+      $content.clone().append(
+        $img.clone().attr('src', imgs[i]),
+        $header.clone().text([headers[i]]),
+        $p.clone().text(text[i]),
+      ).addClass('hidden')
+    
+    );
+    $control.append($circleButton.clone().on('click',()=>{
+      $modal.children('.content').hide();
+      if($modal.children('.content').eq(i).is(':hidden')) $modal.children('.content').eq(i).show().css('display','flex').css('flex-direction','column');
+    }));    
+  }
+  $modal.children('.content').eq(0).toggleClass('hidden');
+  $modal.append($control);
+  
+  $modal.on('click', (event) => event.stopPropagation());
+  $layout.on('click', (event) => {
+    if ($(event.target).is($layout)) {
+      $layout.remove();
+    }
+  });
+  $close.on('click',()=>$layout.remove());
+
+  $('#app').append($layout.append($modal));
+
+  return $modal;
+}
--- src/components/extras/extras.js
+++ src/components/extras/extras.js
@@ -1,3 +1,4 @@
+import { loadExtrasModal } from './extras-modal.js';
 export function loadExtras() {
   const $extras = $('<div>');
   const $h2 = $('<h2>');
@@ -6,18 +7,20 @@ export function loadExtras() {
   const $divItem = $('<div>');
   const $textDiv = $('<div>');
   const $img = $('<img>');
-  const $a=$('<a>');
+  const $a = $('<a>');
 
   $img.height('112');
   $img.width('112');
-  $img.addClass('self-start md:self-center');
+  $img.addClass('self-start md:self-center hover:cursor-pointer');
   $a.addClass('underline hover:cursor-pointer md:text-base text-sm');
   $a.text('Learn more');
   $divItem.addClass(
     'flex flex-row md:flex-col md:text-center place-items-center gap-5'
   );
   $textDiv.addClass('flex flex-col pb-20 gap-4 md:text-base text-sm');
-  $extras.addClass('flex flex-col place-items-center pt-28 gap-5 md:text-center');
+  $extras.addClass(
+    'flex flex-col place-items-center pt-28 gap-5 md:text-center'
+  );
   $extras.append(
     $h2.clone().text('Endless Extras').addClass('font-bold text-3xl'),
     $p
@@ -41,7 +44,8 @@ export function loadExtras() {
                   .clone()
                   .append(
                     'Not only can you earn free coffee, look forward to a birthday treat plus coffee and tea refills.'
-                  ),$a
+                  ),
+                $a.clone()
               )
           ),
         $divItem
@@ -58,7 +62,8 @@ export function loadExtras() {
                   .clone()
                   .append(
                     'Enjoy the convenience of in-store, curbside or drive-thru pickup at select stores.'
-                  ),$a.clone()
+                  ),
+                $a.clone()
               )
           ),
         $divItem
@@ -75,7 +80,8 @@ export function loadExtras() {
                   .clone()
                   .append(
                     'Earn Stars even quicker with Bonus Star challenges, Double Star Days and exciting games.'
-                  ),$a.clone()
+                  ),
+                $a.clone()
               )
           )
       )
@@ -83,5 +89,26 @@ export function loadExtras() {
         'flex flex-col md:flex-row gap-5 px-2 md:px-20 steps md:text-center mt-5'
       )
   );
-  $('#app').append($extras);
+
+  $('#app').append(
+    $extras.on('click', () =>
+      loadExtrasModal(
+        [
+          'src/assets/pngs/A1.png',
+          'src/assets/pngs/A2.png',
+          'src/assets/pngs/A3.png',
+        ],
+        [
+          'Free food, drinks & more',
+          ' Complimentary birthday treat',
+          'Brewed coffee & tea refills on us',
+        ],
+        [
+          'Redeem your Stars for favorites like a handcrafted cold brew, protein box or even a coffee tumbler.',
+          'Every year on your birthday get a free drink or food item of your choice',
+          'More  Caffè Verona® Blend, please. We’re happy to refill your hot cup while you’re in the store. Just ask.'
+        ]
+      )
+    )
+  );
 }
